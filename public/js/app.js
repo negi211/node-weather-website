e881076af74ff42a1b40a4f415f6449e2b413960
negi211 <srj.negi@gmail.com>
2020-07-18 13:59:39 +0530

    setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -12,7 +12,7 @@
         msg1.textContent = 'Loading';
         msg2.textContent = '';
 
-        fetch('http://localhost:3000/weather?address='+location)
+        fetch('/weather?address='+location)
         .then((response)=>{
            response.json().then((data)=>{
                if(data.error)
