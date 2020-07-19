0f970ab4346423990b516adc8934741029d836bb
negi211 <srj.negi@gmail.com>
2020-07-19 16:07:20 +0530

    add image of weather

--- public/js/app.js
+++ public/js/app.js
@@ -4,6 +4,7 @@
    const search = document.querySelector('input');
    const msg1 = document.querySelector('#msg-1');
    const msg2 = document.querySelector('#msg-2');
+   const maincont = document.querySelector('.forecast');
 
    weatherForm.addEventListener('submit',(e)=>{
         e.preventDefault()
@@ -11,6 +12,11 @@
         const location  = search.value;
         msg1.textContent = 'Loading';
         msg2.textContent = '';
+        if(maincont.childElementCount > 1)
+        {
+         maincont.firstElementChild.remove()
+        }
+        
 
         fetch('/weather?address='+location)
         .then((response)=>{
@@ -20,7 +26,11 @@
                 msg1.textContent = data.error   
                }else{
                 msg1.textContent = data.location;
-                msg2.textContent = data.forecast;  
+                var z = data.forecast;  
+                var datadup =  z.split('*');
+                msg2.textContent = datadup[0];
+                var r =  `<img src= ${datadup[1]} width =30 height =30>`;
+                msg2.insertAdjacentHTML('beforebegin',r);
             
                }
             
